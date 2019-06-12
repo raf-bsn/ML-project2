6ac8d837c1169b6a7569adc0378a3bf3d2234520
Bogdan Bakarec <bbakarec16@raf.rs>
2019-06-12 15:48:11 +0200

    Model training finished, minor modifications might be needed. File fashion.h5 is a model with 89.86% test set accuracy, and 0.3 test loss.

--- model.py
+++ model.py
@@ -1,5 +1,9 @@
 import keras
 from keras.datasets import fashion_mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D, BatchNormalization
+from keras.optimizers import SGD
 import numpy as np
 from sklearn.model_selection import StratifiedKFold
 
@@ -21,9 +25,70 @@ print(x_test.shape, y_test.shape)
 # U ovoj sekciji implementirati Keras neuralnu mrezu koja postize tacnost barem
 # 85% na test skupu. Ne menjati fajl van ove sekcije.
 
-# TODO
+x_train = x_train.reshape(10000, 28, 28, 1)
+x_test = x_test.reshape(10000, 28, 28, 1)
+
+print(x_train.shape, y_train.shape)
+print(x_test.shape, y_test.shape)
+
+batch_size = 32
+num_classes = 10
+epochs = 25
+
+model = Sequential()
+model.add(Conv2D(32, (3, 3), padding='same',
+                 input_shape=x_train.shape[1:]))
+model.add(BatchNormalization(axis=3))
+model.add(Activation('relu'))
+model.add(Conv2D(32, (3, 3), padding='same'))
+model.add(BatchNormalization(axis=3))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))  # Max pooling 2x2
+model.add(Dropout(rate=0.25))  # Dropout metoda regularizacije
+
+model.add(Conv2D(64, (3, 3), padding='same'))
+model.add(BatchNormalization(axis=3))
+model.add(Activation('relu'))
+model.add(Conv2D(64, (3, 3), padding='same'))
+model.add(BatchNormalization(axis=3))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(rate=0.25))
+
+model.add(Flatten())
+model.add(Dense(512))  # Prvi potpuno povezan sloj
+model.add(Activation('relu'))
+model.add(Dropout(rate=0.5))
+model.add(Dense(num_classes))  # Finalni potpuno povezan sloj
+model.add(Activation('softmax'))
+
+# Koristimo SGD optimizer
+# opt = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)
+opt = SGD(lr=0.01, momentum=0.9, decay=0.01 / epochs)
+
+# Kompilacija modela
+model.compile(loss='categorical_crossentropy',
+              optimizer=opt,
+              metrics=['accuracy'])
+
+# Normalizacija podataka
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          validation_data=(x_test, y_test),
+          shuffle=True)
+
+# Ispisujemo finalni rezultat na test skupu
+scores = model.evaluate(x_test, y_test, verbose=1)
+print('Test loss:', scores[0])
+print('Test accuracy:', scores[1])
 
 #################################################################################
 
 # Cuvanje istreniranog modela u fajl
-# model.save('fashion.h5')
+model.save('fashion.h5')
