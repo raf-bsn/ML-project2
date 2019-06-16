27313143ade04a3d0b6207473d27077130a253af
Bogdan Bakarec <bbakarec16@raf.rs>
2019-06-16 16:25:59 +0200

    Everything done, documentation and polishing finished.

--- main.py
+++ main.py
@@ -19,27 +19,27 @@ img = cv2.imread('tests/{}.png'.format(tp_idx))
 # Ne menjati fajl van ove sekcije.
 
 # Ucitavamo model
-model = keras.models.load_model('fashion4.h5')
+model = keras.models.load_model('model.h5')
 
 solution = img.copy()
 
+# Removing noise
 dst = cv2.fastNlMeansDenoising(solution,None,50,7,21)
 
-# plt.subplot(121),plt.imshow(solution)
-# plt.subplot(122),plt.imshow(dst)
-# plt.show()
+# src_gray = cv2.cvtColor(dst, cv2.COLOR_BGR2GRAY)
+src_gray = cv2.blur(dst, (3, 3))
 
-src_gray = cv2.cvtColor(dst, cv2.COLOR_BGR2GRAY)
-src_gray = cv2.blur(src_gray, (3,3))
-
-# dst = deepcopy(src_gray)
-# plt.subplot(122),plt.imshow(dst)
-# plt.show()
-
+# Thresholding for easier edge detection
 ret, src_gray = cv2.threshold(src_gray, 240, 255, cv2.THRESH_BINARY)
 
 
 def find_if_close(cnt1, cnt2):
+    """
+    Test whether or not two contours are close to each other
+    :param cnt1: First contour
+    :param cnt2: Second contour
+    :return: True / False
+    """
     row1, row2 = cnt1.shape[0], cnt2.shape[0]
     for i in range(row1):
         for j in range(row2):
@@ -51,14 +51,25 @@ def find_if_close(cnt1, cnt2):
 
 
 def thresh_callback(val):
+    """
+    Does edge detection, after which a contour is created for each object. Contours that are close to each other are
+    merged into one contour by calculating the convex hull. Contours that are completely inside another contour are
+    ignored. Contours are used for calculating all of the bounding boxes which are then drawn on the picture.
+    :param val: Parameter for Canny algorithm, edges with intensity under val are ignored, edges with intensity over
+    2 * val are sure to be edges, edges with intensity between are decided based on connectivity
+    :return: All bounding boxes, indices of bounding boxes to be ignored as they are completely inside other bounding
+    boxes
+    """
     threshold = val
 
+    # Edge detection and finding contours
     canny_output = cv2.Canny(src_gray, threshold, threshold * 2)
     contours, _ = cv2.findContours(canny_output, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     LENGTH = len(contours)
     status = np.zeros((LENGTH, 1))
 
+    # Nearby contours are put in groups
     for i, cnt1 in enumerate(contours):
         x = i
         if i != LENGTH - 1:
@@ -72,6 +83,7 @@ def thresh_callback(val):
                     if status[x] == status[i]:
                         status[x] = i + 1
 
+    # Each group of nearby contours is merged into one by calculating convex hull
     unified = []
     maximum = int(status.max()) + 1
     for i in range(maximum):
@@ -83,81 +95,97 @@ def thresh_callback(val):
 
     contours = unified
 
+    # Bounding box for each contour
     contours_poly = [None] * len(contours)
     boundRect = [None] * len(contours)
-    centers = [None] * len(contours)
-    radius = [None] * len(contours)
     for i, c in enumerate(contours):
         contours_poly[i] = cv2.approxPolyDP(c, 3, True)
         boundRect[i] = cv2.boundingRect(contours_poly[i])
-        centers[i], radius[i] = cv2.minEnclosingCircle(contours_poly[i])
 
+    ignore = []
+
+    # Adding bounding boxes that do not represent real objects to ignored list
+    for i in range(len(boundRect)):
+        for j in range(len(boundRect)):
+            if i == j:
+                continue
+            if boundRect[j][0] >= boundRect[i][0] and boundRect[j][1] >= boundRect[i][1]:
+                if boundRect[j][0] + boundRect[j][2] <= boundRect[i][0] + boundRect[i][2] and \
+                        boundRect[j][1] + boundRect[j][3] <= boundRect[i][1] + boundRect[i][3]:
+                    ignore.append(j)
+
+    # Drawing a rectangle on picture for each bounding box
     for i in range(len(contours)):
         color = (255, 0, 0)
-        latitude = boundRect[i][2] * boundRect[i][3]
-        if latitude < 100:                                      # izbacivanje malih kontura
+        # latitude = boundRect[i][2] * boundRect[i][3]
+        # if latitude < 100:
+        #     continue
+        if i in ignore:
             continue
-        # cv2.drawContours(img, contours_poly, i, color)
         cv2.rectangle(img, (int(boundRect[i][0]), int(boundRect[i][1])), \
                      (int(boundRect[i][0] + boundRect[i][2]), int(boundRect[i][1] + boundRect[i][3])), color, 2)
 
-    return boundRect
+    return boundRect, ignore
 
-# source_window = 'Source'
-# cv2.namedWindow(source_window)
 
-max_thresh = 255
-thresh = 10 # initial threshold
-# cv2.createTrackbar('Canny thresh:', source_window, thresh, max_thresh, thresh_callback)
-boundingBoxes = thresh_callback(thresh)
+thresh = 10
+boundingBoxes, ignore = thresh_callback(thresh)
 
 labelNames = ["tshirt/top", "trouser", "pullover", "dress", "coat", "sandal", "shirt", "sneaker", "bag", "ankle boot"]
 
-print(boundingBoxes)
+# print(boundingBoxes)
 
 for i in range(len(boundingBoxes)):
     imageHeight = boundingBoxes[i][3]
     imageWidth = boundingBoxes[i][2]
-    if imageHeight <= 10 or imageWidth <= 10:
+    # if imageHeight <= 10 or imageWidth <= 10:
+    #     continue
+
+    # Skip bounding boxes inside of other bounding boxes
+    if i in ignore:
         continue
 
+    # Make the bounding box a perfect square so that scaling down to 28x28 results in pictures with higher quality
     if imageWidth > imageHeight:
-        boundingBoxes[i] = (boundingBoxes[i][0], boundingBoxes[i][1] - (imageWidth - imageHeight) // 2, boundingBoxes[i][2], boundingBoxes[i][3])
-        firstImage = solution[boundingBoxes[i][1]: boundingBoxes[i][1] + boundingBoxes[i][2],
-                     boundingBoxes[i][0]: boundingBoxes[i][0] + boundingBoxes[i][2]]
+        boundingBox = (boundingBoxes[i][0], boundingBoxes[i][1] - (imageWidth - imageHeight) // 2, boundingBoxes[i][2], boundingBoxes[i][3])
+        firstImage = solution[boundingBox[1]: boundingBox[1] + boundingBox[2],
+                     boundingBox[0]: boundingBox[0] + boundingBox[2]]
         res = cv2.resize(firstImage, None, fx=28 / imageWidth, fy=28 / imageWidth, interpolation=cv2.INTER_AREA)
     else:
-        boundingBoxes[i] = (boundingBoxes[i][0] - (imageHeight - imageWidth) // 2, boundingBoxes[i][1], boundingBoxes[i][2], boundingBoxes[i][3])
-        firstImage = solution[boundingBoxes[i][1]: boundingBoxes[i][1] + boundingBoxes[i][3],
-                 boundingBoxes[i][0] : boundingBoxes[i][0] + boundingBoxes[i][3]]
+        boundingBox = (boundingBoxes[i][0] - (imageHeight - imageWidth) // 2, boundingBoxes[i][1], boundingBoxes[i][2], boundingBoxes[i][3])
+        firstImage = solution[boundingBox[1]: boundingBox[1] + boundingBox[3],
+                     boundingBox[0] : boundingBox[0] + boundingBox[3]]
         res = cv2.resize(firstImage, None, fx=28 / imageHeight, fy=28 / imageHeight, interpolation=cv2.INTER_AREA)
 
-    # res = np.flip(res, -1)                      # flip vertical, then horizontal
+    # Inverting bits needed because model i trained on black background
     res = cv2.bitwise_not(res)
 
-    # normalization
-    # res = cv2.normalize(res, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+    # Normalization
     res = res.astype('float32')
     res = res / 255
     res = cv2.resize(res, (28, 28))
 
+    # Reshaping to suit model
     res = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
     res = res.reshape(1, 28, 28, 1)
 
+    # Prediction and drawing predicted class
     probabilities = model.predict(res)
-    print(probabilities)
-    prediction = probabilities.argmax(axis=1)
-    label = labelNames[prediction[0]]
-    print(label)
+    # print(probabilities)
+    prediction = probabilities.argmax()
+    label = labelNames[prediction]
+    # print(label)
     font = cv2.FONT_HERSHEY_SIMPLEX
     draw = cv2.putText(img, label, (boundingBoxes[i][0], boundingBoxes[i][1]), font, 0.5, (0, 0, 255), 1,
                        cv2.LINE_AA)
 
-cv2.imshow('Solution: {}'.format(tp_idx), draw)
+solution = draw.copy()
+
+cv2.imshow('Solution: {}'.format(tp_idx), solution)
 
 cv2.waitKey(0)
 
 #################################################################################
 
 # Cuvamo resenje u izlazni fajl
-cv2.imwrite("tests/{}_out4.png".format(tp_idx), draw)
+cv2.imwrite("tests/out_{}.png".format(tp_idx), solution)
--- model.py
+++ model.py
@@ -1,12 +1,9 @@
 import keras
 from keras.datasets import fashion_mnist
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D, BatchNormalization
 from keras.callbacks import ReduceLROnPlateau
-from keras.optimizers import SGD
-from keras.preprocessing.image import ImageDataGenerator
-import numpy as np
 from sklearn.model_selection import StratifiedKFold
 
 # Ucitavanje FashionMNIST skupa podataka
@@ -38,99 +35,42 @@ num_classes = 10
 epochs = 40
 
 model = Sequential()
-model.add(Conv2D(32, (5, 5), padding='same', activation='relu',
-                 input_shape=x_train.shape[1:]))
-# model.add(BatchNormalization(axis=3))
-# model.add(Activation('relu'))
+
+model.add(Conv2D(32, (5, 5), padding='same', activation='relu', input_shape=x_train.shape[1:]))
 model.add(Conv2D(32, (5, 5), padding='same', activation='relu'))
-# model.add(BatchNormalization(axis=3))
-# model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))  # Max pooling 2x2
-model.add(Dropout(rate=0.5))  # Dropout metoda regularizacije
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(rate=0.5))
 
 model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
-# model.add(BatchNormalization(axis=3))
-# model.add(Activation('relu'))
 model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
-# model.add(BatchNormalization(axis=3))
-# model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 model.add(Dropout(rate=0.5))
 
 model.add(Flatten())
-model.add(Dense(512, activation='relu'))  # Prvi potpuno povezan sloj
-# model.add(Activation('relu'))
+model.add(Dense(512, activation='relu'))
 model.add(BatchNormalization())
 model.add(Dropout(rate=0.75))
-model.add(Dense(num_classes, activation='softmax'))  # Finalni potpuno povezan sloj
-# model.add(Activation('softmax'))
+model.add(Dense(num_classes, activation='softmax'))
 
-# Koristimo SGD optimizer
+# RMS propagation
 opt = keras.optimizers.rmsprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=0.0)
-# opt = SGD(lr=0.01, momentum=0.9, decay=0.01 / epochs)
 
-# Kompilacija modela
 model.compile(loss='categorical_crossentropy',
               optimizer=opt,
               metrics=['accuracy'])
 
-# Normalizacija podataka
+# Normalization
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
 x_train /= 255
 x_test /= 255
 
+# Learning rate reduction
 learning_rate_reduction = ReduceLROnPlateau(monitor='val_acc',
                                             patience=3,
                                             verbose=1,
                                             factor=0.5,
                                             min_lr=0.00001)
-
-# datagen = ImageDataGenerator(
-#         featurewise_center=False,  # set input mean to 0 over the dataset
-#         samplewise_center=False,  # set each sample mean to 0
-#         featurewise_std_normalization=False,  # divide inputs by std of the dataset
-#         samplewise_std_normalization=False,  # divide each input by its std
-#         zca_whitening=False,  # apply ZCA whitening
-#         zca_epsilon=1e-06,  # epsilon for ZCA whitening
-#         rotation_range=0,  # randomly rotate images in the range (degrees, 0 to 180)
-#         # randomly shift images horizontally (fraction of total width)
-#         width_shift_range=0.1,
-#         # randomly shift images vertically (fraction of total height)
-#         height_shift_range=0.1,
-#         shear_range=0.,  # set range for random shear
-#         zoom_range=0.,  # set range for random zoom
-#         channel_shift_range=0.,  # set range for random channel shifts
-#         # set mode for filling points outside the input boundaries
-#         fill_mode='nearest',
-#         cval=0.,  # value used for fill_mode = "constant"
-#         horizontal_flip=True,  # randomly flip images
-#         vertical_flip=False,  # randomly flip images
-#         # set rescaling factor (applied before any other transformation)
-#         rescale=None,
-#         # set function that will be applied on each input
-#         preprocessing_function=None,
-#         # image data format, either "channels_first" or "channels_last"
-#         data_format=None,
-#         # fraction of images reserved for validation (strictly between 0 and 1)
-#         validation_split=0.0)
-#
-# # Neke vrednosti potrebne za augmentaciju podataka je neophodno fitovati
-# datagen.fit(x_train)
-#
-# # Fitujemo model na augmentovanim podacima sa 4 workera
-# model.fit_generator(datagen.flow(x_train, y_train,
-#                                  batch_size=batch_size),
-#                     epochs=epochs,
-#                     validation_data=(x_test, y_test),
-#                     steps_per_epoch=x_train.shape[0] // batch_size,
-#                     workers=4)
-
-# model.fit(x_train, y_train,
-#           batch_size=batch_size,
-#           epochs=epochs,
-#           validation_data=(x_test, y_test),
-#           shuffle=True)
 
 model.fit(x_train, y_train,
           batch_size=batch_size,
@@ -139,7 +79,7 @@ model.fit(x_train, y_train,
           verbose=1,
           callbacks=[learning_rate_reduction])
 
-# Ispisujemo finalni rezultat na test skupu
+# Results on test set
 scores = model.evaluate(x_test, y_test, verbose=1)
 print('Test loss:', scores[0])
 print('Test accuracy:', scores[1])
@@ -147,4 +87,4 @@ print('Test accuracy:', scores[1])
 #################################################################################
 
 # Cuvanje istreniranog modela u fajl
-model.save('fashion4.h5')
+model.save('model.h5')
